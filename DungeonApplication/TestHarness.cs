ee9bc0802cf3e1ac19bdd2dc6e1cbd6b50c92978
Luke Kelley <lucas.j.kelley@gmail.com>
2023-02-24 14:40:28 -0600

    made a lot of updates

--- DungeonApplication/TestHarness.cs
+++ DungeonApplication/TestHarness.cs
@@ -11,36 +11,28 @@ namespace DungeonApplication
     {
         static void Main(string[] args)
         {
-            //Build and test the functionality of our library.
-            Character.Header("This is a Test\n\n");
-
             //Build and test a weapon
-            Weapon w1 = new Weapon()
-            {
-                WeaponName = "Maerlyn's Staff",
-                Type = WeaponType.Staff,
-                MaxDamage = 40,
-                MinDamage = 5,
-                BonusHitChance = 4,
-                IsTwoHanded = true,
-            };
-            
-            Console.WriteLine($"{w1.WeaponName}\n" +
-                              $"Is Two-Handed: {w1.IsTwoHanded}\n" +
-                              $"Type: {w1.Type}\n" +
-                              $"Max Damage: {w1.MaxDamage}\n" +
-                              $"Min Damage: {w1.MinDamage}\n" +
-                              $"Bonus Hit Chance: {w1.BonusHitChance}\n");
-
-
+            Console.WriteLine("\n\n+++++++++++++ WEAPON ++++++++++++++++\n");
+            Weapon w1 = new(40, 5, "Maerlyn's Rainbow", 4, true, WeaponType.Orb);
+            Console.WriteLine(w1);
 
             //Build and test a character - include CalcBlock(), CalcHitChance(), CalcDamage()
+            Console.WriteLine("\n++++++++++++++ HERO +++++++++++++++\n");
             Player c1 = new("Hero-Man",100,50,25,Race.Human,w1);
 
-            Console.WriteLine($"{c1}\n" +
-                              $"Damage: {c1.CalcDamage()}\n" +
+            Console.WriteLine(c1);
+
+            Console.WriteLine($"Damage: {c1.CalcDamage()}\n" +
                               $"Hit Chance: {c1.CalcHitChance()}\n" +
                               $"Block: {c1.CalcBlock()}\n");
+
+            //Call to a random Monster
+            Console.WriteLine("\n\n ++++++++++++++ MONSTER +++++++++++++++\n\n");
+            Console.WriteLine(Monster.GetMonster());
+            Monster monster = Monster.GetMonster();
+
+            Console.WriteLine("\n\n ++++++++++++++++++++ COMBAT ++++++++++++++++++++\n\n");
+            Combat.DoBattle(c1, monster);
 
         }
     }
